cf346e133e1e74e0eaa8a5586329d6a4d90ba06f
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-12 04:11:05 +0530

    111

--- jobsearchproject/src/Components/SearchDiv/Search.jsx
+++ jobsearchproject/src/Components/SearchDiv/Search.jsx
@@ -4,9 +4,13 @@ const Search = () => {
   return (
     <div className='searchDiv grid gp-10 bg-grayIsh rounded-[10px] p-[3rem]'>
         <form actipn="">
+
           <div className='firstDiv flex justify-between items-center rounded-[8px] gap-
           [10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
-            
+
+            <div className='flex gap-2 items-center'>
+              
+            </div>
           </div>
         </form>
     </div>
