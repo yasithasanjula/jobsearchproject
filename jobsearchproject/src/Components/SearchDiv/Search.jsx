d190a2a95e00e8d3cbc1e29e82358c8c72f78ed9
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-12 04:32:34 +0530

    1111

--- jobsearchproject/src/Components/SearchDiv/Search.jsx
+++ jobsearchproject/src/Components/SearchDiv/Search.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+//Imported Icons from React Icones===>
+import {AiOutlineSearch} from 'react-icons/ai'
+import {AiOutlineCloseCircle} from 'react-icons/ai'
+
+
 const Search = () => {
   return (
     <div className='searchDiv grid gp-10 bg-grayIsh rounded-[10px] p-[3rem]'>
@@ -9,7 +14,12 @@ const Search = () => {
           [10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
 
             <div className='flex gap-2 items-center'>
-              
+              <AiOutlineSearch className='text-[25px] icon'/>
+              <input type="text" className='bg-transparent text-blue-500
+              focus:outline-none w-[100%]' placeholder='Search Job Here ...' />
+              <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6]
+              hover:text-textcolor icon' />
+
             </div>
           </div>
         </form>
