a76e9701173730c3211149429ca2b81f51dfe4e6
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-12 04:01:24 +0530

    111

--- jobsearchproject/src/Components/SearchDiv/Search.jsx
+++ jobsearchproject/src/Components/SearchDiv/Search.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='searchDiv grid gp-10 bg-grayIsh rounded-[10px] p-[3rem]'>
+        <form actipn="">
+          <div className='firstDiv flex justify-between items-center rounded-[8px] gap-
+          [10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
+            
+          </div>
+        </form>
+    </div>
   )
 }
 
